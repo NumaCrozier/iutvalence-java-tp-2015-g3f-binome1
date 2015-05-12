62e1d422a2dd66d47f46c730a582de4218d50a1c
NumaCrozier <draconk_26@hotmail.fr>
2015-05-12 19:19:00 +0200

    Started making javadoc

--- Motus_refactored/src/motus/iutvalence/tp3F/Grid.java
+++ Motus_refactored/src/motus/iutvalence/tp3F/Grid.java
@@ -4,20 +4,40 @@ import java.util.*;
 
 public class Grid 
 {
+	/**
+	 * The grid that will be created and in which will be placed letters
+	 */
 	private char[] gameGrid;
+	
+	/**
+	 * The size of the game grid (in number of characters).
+	 */
 	private int gridSize;
 
+	/**
+	 * Constructor for the Grid class.
+	 */
 	public Grid()
 	{
 		this.gridSize = randomGridSizeChoice();
 		this.gameGrid = randomLetterChoice(this.gridSize);
 	}
 	
+	/**
+	 * Method to get the game grid.
+	 * @return char[]
+	 */
 	public char[] getGameGrid()
 	{
 		return this.gameGrid;
 	}
 	
+	/**
+	 * Method that compare a character to the gamegrid, to know if it's in or not.
+	 * @param characterToCompare ( char )
+	 * @param gridToCover ( char[] )
+	 * @return boolean
+	 */
 	public boolean isInTheGrid(char characterToCompare, char[] gridToCover)
 	{
 		for(int gridCover = 0 ; gridCover < gridToCover.length ; gridCover++)
@@ -28,11 +48,20 @@ public class Grid
 		return false;
 	}
 	
+	/**
+	 * Method returning a character from it's position in the game grid.
+	 * @param characterNumero ( int )
+	 * @return char
+	 */
 	public char getCharacterFromGameGrid(int characterNumero)
 	{
 		return this.gameGrid[characterNumero];
 	}
 	
+	/**
+	 * Method that displays the game grid, but where letters are replaced by stars so the player can't see the word he has to find.
+	 * @param grilleDeJeu ( Grid )
+	 */
 	public static void displayHiddenGrid(Grid grilleDeJeu)
 	{
 		System.out.println("Grille : ");
@@ -43,13 +72,22 @@ public class Grid
 		System.out.println();
 	}
 
+	/**
+	 * Method that will chose the game grid's size randomly (between 3 and 8)
+	 * @return int
+	 */
 	public int randomGridSizeChoice() 
 	{
 		Random randomSizeChoice = new Random();
-		int choixTaille = (randomSizeChoice.nextInt(8)+1);
+		int choixTaille = (randomSizeChoice.nextInt(8)+3);
 		return choixTaille;
 	}
 
+	/**
+	 * Method that returns a game grid completed with random letters. Makes the game harder, that's fun.
+	 * @param length
+	 * @return char[]
+	 */
 	public char[] randomLetterChoice(int length) 
 	{
 		char randomGameGrid[] = new char[length] ;
--- Motus_refactored/src/motus/iutvalence/tp3F/Main.java
+++ Motus_refactored/src/motus/iutvalence/tp3F/Main.java
@@ -2,6 +2,10 @@ package motus.iutvalence.tp3F;
 
 public class Main 
 {
+	/**
+	 * Main method, where the program launching will be put.
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		Game.start();		
