c2ed67a3417172ad3bd4f6b8769e38104a46df27
Batlord <antonin.metzler@laposte.net>
2015-05-24 22:42:05 +0200

    rajout des panels
    à faire : les mettres dans la fenetre et faire les tests de mise en
    forme

--- Motus_refactored/src/motus/iutvalence/tp3F/view/MotusGameWindow.java
+++ Motus_refactored/src/motus/iutvalence/tp3F/view/MotusGameWindow.java
@@ -1,19 +1,24 @@
 package motus.iutvalence.tp3F.view;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import javax.swing.JFrame;	
+import javax.swing.JPanel;
 
 public class MotusGameWindow {
 		public static void main(String[] args)
 		{
 			JFrame windowMotus = new JFrame();
+			JPanel eastPanel = new JPanel(); 
+			JPanel centerPanel = new JPanel(); 
+			JPanel mainPanel = new JPanel(); 
+			
 			windowMotus.setVisible(true);
 			windowMotus.setSize(600, 400);
 			windowMotus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			windowMotus.setTitle("Motus");
 			windowMotus.setLocationRelativeTo(null);
 			windowMotus.setLayout(new BorderLayout());
+			
 			
 		}
 		
