813009f85c01ed98729836cfb5490d66031a5635
Batlord <antonin.metzler@laposte.net>
2015-05-25 22:02:16 +0200

    Panel west completé
    à faire : completer le Panel centre

--- Motus_refactored/src/motus/iutvalence/tp3F/view/MotusGameWindow.java
+++ Motus_refactored/src/motus/iutvalence/tp3F/view/MotusGameWindow.java
@@ -1,23 +1,57 @@
 package motus.iutvalence.tp3F.view;
 
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;	
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MotusGameWindow {
 		public static void main(String[] args)
 		{
 			JFrame windowMotus = new JFrame();
-			JPanel eastPanel = new JPanel(); 
-			JPanel centerPanel = new JPanel(); 
+			
+			JPanel westPanel = new JPanel(); 
+			JPanel centerPanel = new JPanel();
+			JPanel proposedWordPanel = new JPanel();
 			JPanel mainPanel = new JPanel(); 
 			
-			windowMotus.setVisible(true);
+			JLabel nickname = new JLabel("Player's nickname");
+			JLabel nbTurns = new JLabel("The number of turns left");
+			
+			JTextField proposedWord = new JTextField("Propose a word");
+			
+			JButton submitWord = new JButton("Submit");
+						
+//===================================================================================
+			windowMotus.setTitle("Motus");
 			windowMotus.setSize(600, 400);
 			windowMotus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			windowMotus.setTitle("Motus");
 			windowMotus.setLocationRelativeTo(null);
-			windowMotus.setLayout(new BorderLayout());
+			windowMotus.setVisible(true);
+			
+			mainPanel.setLayout(new GridLayout(1, 2));
+			mainPanel.add(westPanel);
+			mainPanel.add(centerPanel);
+			
+			westPanel.setBorder(BorderFactory.createTitledBorder("User"));
+			westPanel.setLayout(new GridLayout(6, 1));
+			westPanel.add(nickname);
+			westPanel.add(nbTurns);
+			westPanel.add(proposedWordPanel);
+			
+			proposedWord.setColumns(17);
+			proposedWordPanel.add(proposedWord);
+			proposedWordPanel.add(submitWord);
+			
+			centerPanel.setBorder(BorderFactory.createTitledBorder("Game"));
+			
+			windowMotus.setContentPane(mainPanel);
+			
+			
 			
 			
 		}
