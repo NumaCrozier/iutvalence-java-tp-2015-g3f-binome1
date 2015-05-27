a0c28c4731dc1602ffe34ebf69a8f5d6741cefbb
Batlord <antonin.metzler@laposte.net>
2015-05-27 22:12:34 +0200

    search for the display of the word

--- Motus_refactored/src/motus/iutvalence/tp3F/view/MotusGameWindow.java
+++ Motus_refactored/src/motus/iutvalence/tp3F/view/MotusGameWindow.java
@@ -1,26 +1,31 @@
 package motus.iutvalence.tp3F.view;
 
-import java.awt.Color;
 import java.awt.GridLayout;
 
-import javax.security.auth.login.CredentialExpiredException;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;	
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
 import javax.swing.JTextField;
 
 public class MotusGameWindow {
 	
+	private int col=0;
 	 
+	public int getCol() {
+		return col;}
+
+	public void setCol(int col) {
+		this.col = col;}
+
 		public static void main(String[] args)
 		{
 			JFrame windowMotus = new JFrame();
 			
 			JPanel westPanel = new JPanel(); 
-			JPanel centerPanel = new JPanel();
+			JPanel eastPanel = new JPanel();
 			JPanel proposedWordPanel = new JPanel();
 			JPanel mainPanel = new JPanel();
 			JPanel wordPanel = new JPanel();
@@ -28,20 +33,31 @@ public class MotusGameWindow {
 			JLabel nickname = new JLabel("Player's nickname");
 			JLabel nbTurns = new JLabel("The number of turns left");
 			JLabel wordLength = new JLabel("The length of the word");
+			/*JLabel word = new JLabel("WORD");
+			JLabel word2 = new JLabel("WORD");
+			JLabel word3 = new JLabel("WORD");
+			JLabel word4 = new JLabel("WORD");
+			JLabel word5 = new JLabel("WORD");
+			JLabel word6 = new JLabel("WORD");
+			JLabel word7 = new JLabel("WORD");
+			JLabel word8 = new JLabel("WORD");
+			JLabel word9 = new JLabel("WORD");
+			JLabel word10 = new JLabel("WORD");*/
 			
 			JTextField proposedWord = new JTextField("Propose a word");
 			
 			JButton submitWord = new JButton("Submit");					
 //===================================================================================
 			windowMotus.setTitle("Motus");
-			windowMotus.setSize(600, 400);
+			windowMotus.setSize(800, 400);
 			windowMotus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			windowMotus.setLocationRelativeTo(null);
+			windowMotus.setResizable(false);
 			windowMotus.setVisible(true);
 			
 			mainPanel.setLayout(new GridLayout(1, 2));
 			mainPanel.add(westPanel);
-			mainPanel.add(centerPanel);
+			mainPanel.add(eastPanel);
 			
 			westPanel.setBorder(BorderFactory.createTitledBorder("User"));
 			westPanel.setLayout(new GridLayout(6, 1));
@@ -54,17 +70,21 @@ public class MotusGameWindow {
 			proposedWordPanel.add(proposedWord);
 			proposedWordPanel.add(submitWord);
 			
-			centerPanel.setBorder(BorderFactory.createTitledBorder("Game"));
-			centerPanel.add(wordPanel);
+			eastPanel.setBorder(BorderFactory.createTitledBorder("Game"));
+			eastPanel.add(wordPanel);
 			
-			Object[][] data = {
-				      {"", "", "", "", "", "", "", "", "", ""},
-				    };
-			
-			String  title[] = {"", "", "", "", "", "", "", "", "", ""};
-		    JTable tableau = new JTable(data, title);
-		    wordPanel.add(tableau);
-
+			wordPanel.setLayout(new GridLayout(10,1));
+			/*wordPanel.add(word);
+			wordPanel.add(word2);
+			wordPanel.add(word3);
+			wordPanel.add(word4);
+			wordPanel.add(word5);
+			wordPanel.add(word6);
+			wordPanel.add(word7);
+			wordPanel.add(word8);
+			wordPanel.add(word9);
+			wordPanel.add(word10);*/
+		
 			windowMotus.setContentPane(mainPanel);
 
 			
